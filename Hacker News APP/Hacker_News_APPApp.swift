85af9bc7b4a1ecb887045af3f3e8f378da4d8ca9
Ahmed Nagy Radwan <ahmed.nagy.radwan@gmail.com>
2021-03-18 11:21:13 +0200

    Initial Commit

---
+++ Hacker News APP/Hacker_News_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_APPApp.swift
+//  Hacker News APP
+//
+//  Created by Nagy on 18/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
